a071ffd50ffeabd325a9c2bda9cc741bbabf125d
sungm958 <99030811+sungm958@users.noreply.github.com>
2022-05-12 16:23:11 -0400

    Add files via upload

---
+++ image.js
@@ -0,0 +1,31 @@
+var myImage = document.getElementById('mainImage');
+image1.onclick = function(){
+	myImage.setAttribute("src", "jinglebell.jpeg");
+}
+image2.onclick = function(){
+	myImage.setAttribute("src", "twinkle.jpeg");
+}
+image3.onclick = function(){
+	myImage.setAttribute("src", "iloveu.jpeg");
+}
+image4.onclick = function(){
+	myImage.setAttribute("src", "hbd.jpeg");
+}
+image5.onclick = function(){
+	myImage.setAttribute("src", "row.jpeg");
+}
+image6.onclick = function(){
+	myImage.setAttribute("src", "http://androidhotspot.com/demo/image6.jpg");
+}
+image7.onclick = function(){
+	myImage.setAttribute("src", "http://androidhotspot.com/demo/image7.jpg");
+}
+image8.onclick = function(){
+	myImage.setAttribute("src", "http://androidhotspot.com/demo/image8.jpg");
+}
+image9.onclick = function(){
+	myImage.setAttribute("src", "http://androidhotspot.com/demo/image9.jpg");
+}
+image10.onclick = function(){
+	myImage.setAttribute("src", "http://androidhotspot.com/demo/image10.jpg");
+}
